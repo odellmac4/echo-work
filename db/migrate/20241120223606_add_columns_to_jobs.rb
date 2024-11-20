38fcb4f2a0fa702161cf6170d506b1e6f4fe9bb1
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-20 14:40:55 -0800

    feat: Update jobs table to include location, date_posted, and industry columns

---
+++ db/migrate/20241120223606_add_columns_to_jobs.rb
@@ -0,0 +1,7 @@
+class AddColumnsToJobs < ActiveRecord::Migration[7.1]
+  def change
+    add_column :jobs, :location, :string
+    add_column :jobs, :date_posted, :string
+    add_column :jobs, :industry, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_164217) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_20_223606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_164217) do
     t.boolean "contract"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "date_posted"
+    t.string "industry"
   end
 
   create_table "posts", force: :cascade do |t|
