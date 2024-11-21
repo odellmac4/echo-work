7a820c69c98d3f0a94832e2283ea6d6a77490640
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-21 10:47:50 -0800

    test: Add association and validation testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:comment_text) }
+  end
+
+  describe 'associations' do
+    it { should belong_to :post }
+    it { should belong_to :user }
+  end
 end
