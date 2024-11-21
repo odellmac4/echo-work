f2267d02fdd4bca3cda6ff4f85a33f48948b94ab
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-21 10:50:41 -0800

    test: Add validation and association testing for reply model

--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Reply, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:reply_text) }
+  end
+
+  describe 'associations' do
+    it { should belong_to :user }
+    it { should belong_to :comment }
+  end
 end
