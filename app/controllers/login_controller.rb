b68bd95d26e64560d6b9a3c0c0cd726883567180
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-21 10:08:17 -0800

    feat: Add LoginController to handle basic auth logic for user login

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,20 @@
+class LoginController < ApplicationController
+  def index
+    render "/login"
+  end
+
+  def user_login
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      redirect_to "/home"
+    else
+      render "/login", status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password)
+  end
+end
