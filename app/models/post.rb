0e48cd49f91bf8607231f9a3b2492d121ab44707
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-22 11:17:10 -0800

    feat: Update association for post to have a job association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   validates :likes, :dislikes, :post_text, presence: true
   validates :likes, :dislikes, numericality: true
 
+  has_one :job
   has_many :comments
   has_many :replies, through: :comments
 end
