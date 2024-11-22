27e8abd2075c6e6dd32e924e45ccb0e1dc07f3bb
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-22 14:55:46 -0800

    feat: Add before action for adding a users username to a comment that they leave

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,9 @@ class Comment < ApplicationRecord
   has_many :replies
 
   validates :comment_text, presence: true
+  before_save :set_username
+
+  def set_username
+    self.username = comment.user.username
+  end
 end
