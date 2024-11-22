4a54f1a9429715a5360e278bc89f6fcbe0bc74ea
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-22 14:55:21 -0800

    feat: Add path for PostShow component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "./components/Login";
 import "./components/Home";
+import "./components/PostShow";
 import "../assets/stylesheets/application.tailwind.css";
