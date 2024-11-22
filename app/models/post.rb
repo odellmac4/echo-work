9e7a7de1d4c409ddd32a21761c36062cbb14c0c9
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-22 10:34:58 -0800

    feat: Add validations and associations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :likes, :dislikes, :post_text, presence: true
+  validates :likes, :dislikes, numericality: true
+
+  has_many :comments
+  has_many :replies, through: :comments
 end
