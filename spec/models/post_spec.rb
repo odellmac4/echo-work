cf08dfd2b251a9feb64a4aa1871c0fcc079a1e32
Jack Sweeney <jackcsweeney@gmail.com>
2024-11-22 11:19:21 -0800

    clean up: remove unnecessary whiteboarding in post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,8 +14,3 @@ RSpec.describe Post, type: :model do
     it { should have_many(:replies).through(:comments) }
   end
 end
-Job.create!({job_title: "Software Engineer, TikTok Brand Ads", company_name: "TikTok", job_description: "test job description", qualifications: "test qualifications", salary: "$130,000", full_time: true, contract: false, location: "San Francisco, CA", date_posted: "11/22/24", industry: "Entertainment Providers"})
-Post.create!({job_id: 1, post_text: "test post text", likes: 3, dislikes: 14})
-User.create!({name: "Test", email: "test@email.com", username: "TestUsername", password: "test", password_confirmation: "test"})
-Comment.create!({post_id: 1, user_id: 1, comment_text: "Test comment text."})
-Reply.create!({user_id: 1, comment_id: 1, reply_text: "Test reply text."})
