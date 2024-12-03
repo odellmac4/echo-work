9b3331fe75b0ff01eb4e6225f24c4379e5868184
Jack Sweeney <jackcsweeney@gmail.com>
2024-12-03 11:57:52 -0800

    feat: Separate Comment component from PostShow component for organizational purposes

--- app/javascript/components/Comment.jsx
+++ app/javascript/components/Comment.jsx
@@ -1,42 +1,46 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import ReactDOM from "react-dom/client";
+import axios from 'axios';
+import Reply from "./Reply";
 
-class Comment extends React.Component {
-
-
-  render(){
-    return(
-      <div>
-        <div className="chat chat-start">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS chat bubble component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
+export default function Comment({ post }) {
+  const postId = post.id
+  const [repliesOpened, setRepliesOpened] = useState(false)
+  const [loadedReplies, setLoadedReplies] = useState([])
+  const [replyText, setReplyText] = useState('')
+  const token = document.querySelector('[name=csrf-token]').content 
+  
+  function handleClick(commentId) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token
+    axios.get(`/api/v1/posts/${postId}/comments/${commentId}/replies`)
+      .then(data => {
+        console.log(data)
+        setLoadedReplies(data.data.replies)
+        setRepliesOpened(true)
+      })
+      .catch(error => console.log(error))
+  }
+    
+  return post.comments.map((comment, index) =>
+    <div>
+      <div className="mt-4 space-y-4" key={index}>
+        <div className="bg-gray-700 p-4 rounded-md space-y-2">
+          <div className="flex justify-between">
+            <div className="font-semibold">{comment.user_id}</div>
+            <div className="text-gray-400 text-sm">{comment.created_at}</div>
           </div>
-          <div className="chat-header">
-            Obi-Wan Kenobi
-          </div>
-          <div className="chat-bubble">You were the Chosen One!</div>
+          <p>{comment.comment_text}</p>
         </div>
-        <div className="chat chat-end">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS chat bubble component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-          </div>
-          <div className="chat-header">
-            Anakin
-          </div>
-          <div className="chat-bubble">I hate you!</div>
+        <div>
+          <button onClick={e => handleClick(e.target.value)} value={comment.id}>Load Replies</button>
+          {repliesOpened && <Reply replies={loadedReplies} />}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+
 
 const commentContainer = document.getElementById("comment-container")
 if (commentContainer) {
